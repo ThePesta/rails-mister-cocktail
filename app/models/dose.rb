c4912a7f43de1e97346f1364df806cfad65ea228
Jason Cheung <jckaichu@gmail.com>
2017-05-25 11:52:47 +0100

    removed bleh column

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, :cocktail, :ingredient, presence: true
-  validates_uniqueness_of :cocktail, scope: [:ingredient, :bleh]
+  validates_uniqueness_of :cocktail, scope: [:ingredient]
 end
